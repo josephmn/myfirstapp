b06967e820c8a259ea4795c2019c3fffaf2d30b5
joseph magallanes <josephcarlos.jcmn@gmail.com>
2022-06-05 10:17:19 -0500

    aprendiendo funciones nuevas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
+import { Data } from '@angular/router';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  users = ['ryan','joe','cameron','jhon','joseph','joel','kevin','laisson'];
+  users: string[] = ['ryan','joe','cameron','jhon','joseph','joel','kevin','laisson',];
+  activated = true;
+
+  posts: any[] = []; // any cuando tienes "noImplicitAny": false en tsconfig.json 
+
+  // name: string = 'Ryan Ray';
+  // age: number;
+  // address: {
+  //   street: string;
+  //   city: string;
+  // };
+  // hobbies: string[];
+
+  // constructor() {
+  //   this.age = 28;
+  //   this.address = {
+  //     street: '2218 Back street',
+  //     city: 'London',
+  //   };
+  //   this.hobbies = ['swimming', 'read', 'write'];
+  // }
+
+  // constructor data services
+  constructor(private dataService: DataService) {
+    this.dataService.getData().subscribe(data => {
+      // console.log(data);
+      this.posts = data;
+    })
+  }
+
+  // para evento click en boton
+  sayHello() {
+    alert('Hello!');
+  }
+
+  // evento agregar usuario
+  addUser(newUser) {
+    console.log(newUser.value);
+    this.users.push(newUser.value);
+    newUser.value = '';
+    return false;
+  }
+  // evento eliminar
+  deleteUser(user) {
+    for (let i = 0; i < this.users.length; i++) {
+      if (user == this.users[i]) {
+        this.users.splice(i, 1);
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms"; // binding para datos
+import { HttpClientModule } from "@angular/common/http"; // cliente http module
 
 import { AppComponent } from './app.component';
 import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
+import { UserComponent } from './user/user.component';
+
+// import services
+import { DataService } from "./data.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HolaMundoComponent
+    HolaMundoComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, //modulo usado para binding
+    HttpClientModule //modulo para http cliente modulo
   ],
-  providers: [],
+  providers: [DataService], // aqui se declare el services
   bootstrap: [AppComponent]
 })
 export class AppModule { }
